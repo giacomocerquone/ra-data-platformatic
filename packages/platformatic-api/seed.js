8fbf5f5d1d173b3f65ef50a0c2707d9e821a37b8
giacomocerquone <cerquone96@hotmail.it>
2022-10-16 16:13:20 +0200

    feat(api): add gender property

--- packages/platformatic-api/seed.js
+++ packages/platformatic-api/seed.js
@@ -1,22 +1,26 @@
 const users = [
   {
     username: "Dorothy Gale",
+    gender: "Female",
   },
   {
     username: "Martin Brody",
+    gender: "Male",
   },
   {
     username: "Han Solo",
+    gender: "Male",
   },
   {
     username: "Blanche DuBois",
+    gender: "Female",
   },
 ];
 
 module.exports = async function ({ entities, db, sql }) {
   for (const values of users) {
     const user = await entities.user.save({
-      input: { username: values.username },
+      input: values,
     });
 
     console.log("Created user:", user);
